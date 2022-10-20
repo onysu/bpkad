e1523901926d681aa7574721773ccca229f2da5f
ony <onydarrell@gmail.com>
2022-10-20 23:01:05 +0700

    M

--- layanan/koneksi.php
+++ layanan/koneksi.php
@@ -3,6 +3,6 @@ $koneksi = mysqli_connect("localhost", "root", "", "db_upload_foto");
 
 // Check connection
 if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
 }
