261d5cf207f41c62b6d128314a3cd762becf414c
ony <onydarrell@gmail.com>
2022-10-22 02:09:14 +0700

    M

--- admin/info-opd/upload.php
+++ admin/info-opd/upload.php
@@ -5,7 +5,7 @@ $dokumen = $_FILES['dokumen']['name'];
 $tmp_dokumen = $_FILES['dokumen']['tmp_name'];
 $keterangan = $_POST['keterangan'];
 $jenis_file = $_POST['file'];
-move_uploaded_file($tmp_dokumen, 'files/' . $dokumen);
+move_uploaded_file($tmp_dokumen, '../../files/' . $dokumen);
 $query = "INSERT INTO infoopd SET keterangan='$keterangan', file='$jenis_file', dokumen='$dokumen'";
 mysqli_query($koneksi, $query);
 header('location:index.php');
