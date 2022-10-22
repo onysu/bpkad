c1a33565b9704dc515dab1ec1833779e20f0860b
ony <onydarrell@gmail.com>
2022-10-22 10:24:02 +0700

    Update header.php

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -5,12 +5,12 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="../assets/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
   <!-- bootstrap 5.2.2 -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../assets/dist/css/adminlte.min.css">
-  <link rel="stylesheet" href="../assets/css/cssku.css">
+  <link rel="stylesheet" href="assets/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="assets/css/cssku.css">
   <!-- dropzone -->
-  <link rel="stylesheet" href="../assets/plugins/dropzone/min/dropzone.min.css">
+  <link rel="stylesheet" href="assets/plugins/dropzone/min/dropzone.min.css">
 </head>
